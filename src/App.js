6bf44d4f8b413beac35a1b39e9592c3f9069f820
hoshi <tatsuya.hoshi@salto.link>
2024-07-24 02:14:21 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import { Header } from "./component/header/Header.js";
 import { Footer } from "./component/footer/Footer.js";
+import { Page1 } from "./component/Page1";
+import { Page2 } from "./component/Page2";
+import { Page3 } from "./component/Page3";
+import { BrowserRouter, Link, Switch, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -9,7 +13,12 @@ function App() {
       <header className="App-header">
         <Header/>
         <img src={logo} className="App-logo" alt="logo" />
-        <Footer/>
+          <BrowserRouter>
+              <div className="App">
+                  {/* <Footer /> */}
+              </div>
+          </BrowserRouter>
+          <Footer/>
       </header>
     </div>
   );
